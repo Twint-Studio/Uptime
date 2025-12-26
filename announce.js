bdbad0279c91d264fb9549d7d14303c3fd09b437
Prince527 <Prince527gaming@gmail.com>
2025-12-26 13:48:15 -0400

    Patch

--- announce.js
+++ announce.js
@@ -1,8 +1,6 @@
 const fs = require("fs/promises");
 
 const FILE = "./src/assets/json/announce.json";
-const ACTIONS = ["opened", "edited", "labeled", "unlabeled", "reopened"];
-const COMMON = ["info",  "success", "warning", "error"];
 
 async function main() {
     const event = process.env.GITHUB_EVENT_PATH;
@@ -11,13 +9,12 @@ async function main() {
     const { action, issue } = require(event);
     if (!issue?.title) return;
 
-    if (action === "deleted") return await fs.writeFile(FILE, "{}\n");
-
-    if (!ACTIONS.includes(action)) return;
+    if (["deleted", "closed"].includes(action)) return await fs.writeFile(FILE, "{}\n");
+    if (!["edited", "labeled"].includes(action)) return;
 
     const first = issue.labels?.[0];
     const label = (typeof first === "string" ? first : first?.name)?.toLowerCase();
-    const type = COMMON.includes(label) ? label : label || "info";
+    const type = ["info",  "success", "warning", "error"].includes(label) ? label : label || "info";
 
     await fs.writeFile(FILE, JSON.stringify({ name: issue.title, type }, null, 4));
 
